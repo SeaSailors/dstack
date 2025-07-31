d3f136bceb1160d705ae914bd4136af2e13d17c3
SeaSailors <130479086+SeaSailors@users.noreply.github.com>
2025-07-31 19:07:26 +0800

    chore: demo cert event log

--- cert-client/src/lib.rs
+++ cert-client/src/lib.rs
@@ -100,9 +100,10 @@ impl CertRequestClient {
         let report_data = QuoteContentType::RaTlsCert.to_report_data(&pubkey);
         let (quote, event_log) = if !no_ra {
             let (_, quote) = get_quote(&report_data, None).context("Failed to get quote")?;
-            let event_log = read_event_logs().context("Failed to decode event log")?;
-            let event_log =
-                serde_json::to_vec(&event_log).context("Failed to serialize event log")?;
+            // let event_log = read_event_logs().context("Failed to decode event log")?;
+            // let event_log =
+            //     serde_json::to_vec(&event_log).context("Failed to serialize event log")?;
+            let event_log = vec![];
             (quote, event_log)
         } else {
             (vec![], vec![])
